bf49fe2cb3bbdc06cdb97e1aa4ecdd2cb95be92e
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-07-28 18:29:23 +0200

    Added false test

--- src/test/java/com/example/circleci/controller/HelloControllerTest.java
+++ src/test/java/com/example/circleci/controller/HelloControllerTest.java
@@ -10,38 +10,46 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ContextConfiguration(classes = {HelloController.class})
 @ExtendWith(SpringExtension.class)
 class HelloControllerTest {
-	@Autowired
-	private HelloController helloController;
+    @Autowired
+    private HelloController helloController;
 
-	/**
-	 * Method under test: {@link HelloController#sayHello()}
-	 */
-	@Test
-	void testSayHello() throws Exception {
-		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
-		MockMvcBuilders.standaloneSetup(helloController)
-				.build()
-				.perform(requestBuilder)
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-				.andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
-	}
+    /**
+     * Method under test: {@link HelloController#sayHello()}
+     */
+    @Test
+    void testSayHello() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
+        MockMvcBuilders.standaloneSetup(helloController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
 
-	/**
-	 * Method under test: {@link HelloController#sayHello()}
-	 */
-	@Test
-	void testSayHello2() throws Exception {
-		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/", "Uri Variables");
-		MockMvcBuilders.standaloneSetup(helloController)
-				.build()
-				.perform(requestBuilder)
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-				.andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
-	}
+    /**
+     * Method under test: {@link HelloController#sayHello()}
+     */
+    @Test
+    void testSayHello2() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/", "Uri Variables");
+        MockMvcBuilders.standaloneSetup(helloController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+
+
+    @Test
+    void dsf() {
+        assertEquals(5, 4);
+    }
 }
 
