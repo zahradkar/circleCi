3edb196f092e46e0be736bf1afb97e897f794ac2
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-07-28 11:58:35 +0200

    Initial commit

---
+++ src/main/java/com/example/circleci/CircleCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circleci/CircleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
